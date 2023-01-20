45140eaec0ae999f3fc3c8a67282bf91866a3b50
Ungs <dkfnfn7893@naver.com>
2023-01-20 20:02:19 +0900

    연도, 월, 일을 입력하면 해당 요일을 출력해주는 프로그램

---
+++ CalendarEx/src/Main.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("연도를 입력하세요 >> ");
+        int year = in.nextInt();
+        System.out.print("월을 입력하세요 >> ");
+        int month = in.nextInt();
+        System.out.print("일을 입력하세요 >> ");
+        int day = in.nextInt();
+
+        Calendar c = new Calendar(year, month, day);
+
+        System.out.println("해당 날짜의 요일은 " + c.d_o_week() + "입니다.");
+    }
+}
