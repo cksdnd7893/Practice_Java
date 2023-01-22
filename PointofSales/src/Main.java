69311a163fd6f304cef45bb31d958255d9996f31
Ungs <dkfnfn7893@naver.com>
2023-01-22 19:16:48 +0900

    POS기 시스템

---
+++ PointofSales/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        DBManager dbm = new DBManager();
+
+        System.out.println("1. 메뉴관리");
+        System.out.println("하시려는 작업의 번호를 입력하세요 >> ");
+
+    }
+}
